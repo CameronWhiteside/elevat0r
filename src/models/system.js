987395748f3ef138fa824e20c3cd00e51afa978f
Cameron Whiteside <whiteside.cameron@gmail.com>
2022-05-06 23:13:36 -0600

    added matrix specification

--- src/models/system.js
+++ src/models/system.js
@@ -83,8 +83,27 @@ export default class System {
         this.assignElevatorToPickup(request)
     }
 
-
-
+    getNearestFloorMatrix() {
+        let matrix = []
+        for (let i = 0; i < this.floorCount; i++) {
+            let row = []
+            for (let j = 0; j < this.elevators.length; j++) {
+                let elevator = this.elevators[j]
+                if (Math.floor(elevator.position + 0.5) === i) {
+                    let destination = i;
+                    if (elevator.stops.head.level) {
+                        destination = elevator.stops.head.level
+                    }
+                    row.push(destination.toString())
+                } else {
+                    row.push(
+                        ('-')
+                    )
+                }
+            }
+            matrix.push(row)
+        }
+    }
 }
 
 
